07d6feb483552542a18e6824e847b09bd8922daf
Darryl King <darrylking15@gmail.com>
2020-07-09 21:20:58 -0400

    first commit

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first (arr){
+  return arr.shift();
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+   return arr.pop();
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -49,7 +53,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+function looper(family){
+    for(i = 0; i < family.length; i++){
+      alert(family[i])
+    }
 
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -63,6 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for(i = letters.length - 1; i >=0 ; i--){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -79,7 +93,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(nums){
+    let evenNum = []
+    for(i = 0; i < nums.length; i++){
+      if(nums[i] % 2 === 0){
+        evenNum.push(nums[i])
+      }
+    }
+    return evenNum
+}
 
 
 
@@ -107,7 +129,9 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-
+function divider(){
+  
+}
 
 
 ////////// PROBLEM 7 //////////
--- callbacks/practice.js
+++ callbacks/practice.js
@@ -28,7 +28,9 @@
 */
 
 // Code Here 
-
+function first (array, cb){
+    cb(array[0])
+}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -48,7 +50,9 @@ first(names, function(firstName){
 */
 
 //Code Here
-
+function last(arr, cb){
+  cb(arr.pop())
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,7 +70,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1, num2, cb){
+  cb(num1 * num2)
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +91,13 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
-
+function contains (arr, name, cb){
+  if(arr.includes(name)){
+      cb(true)
+  } else {
+      cb(false)
+  }
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -106,6 +118,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb){
+  let arr2 = []
+  for( let i = 0; i < arr.length; i++){
+    if(arr2.indexOf(arr[i]) === -1){
+      arr2.push(arr[i])
+    }
+    cb(arr2)
+  }
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,7 +144,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+function each(arr, cb){
+  for( let i = 0; i < arr.length; i++){
+    cb(name, i)
+  }
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,7 +165,13 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+function getUserById(users, id , cb ){
+  for( let i = 0; i < arr.length; i++){
+    if(users === users){
+      cb(users)
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
